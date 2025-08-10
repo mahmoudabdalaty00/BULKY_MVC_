cc2a724adf5ecf363bd428ac142f4d91898ef3bd
mahmoudabdalaty00 <117662685+mahmoudabdalaty00@users.noreply.github.com>
2025-08-10 11:48:19 +0300

    Add Base IRepository and Repository

---
+++ Bulky.DataAccess/Repository/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> queries = dbSet;
+            return queries.ToList();
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            IQueryable<T> queries = dbSet;
+            var query = queries.Where(expression);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+           dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+       
+
+    }
+}
