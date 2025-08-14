174a237a9f13aa7bf325e55863320ef981cea0b8
mahmoudabdalaty00 <117662685+mahmoudabdalaty00@users.noreply.github.com>
2025-08-14 09:47:45 +0300

    #Add user role management and locking functionality Updated UserController to manage user roles with UserManager. Introduced RoleManagement action and view, and replaced delete method with lock/unlock functionality. Created RoleManagmentVM for role management. Enhanced UI with TinyMCE and Toastr scripts. Updated user.js for dynamic locking/unlocking buttons.

--- BULKYWEB/Areas/Admin/Controllers/UserController.cs
+++ BULKYWEB/Areas/Admin/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using Blky.Utility;
 using Bulky.DataAccess.Data;
 using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BULKYWEB.Areas.Admin.Controllers
@@ -12,9 +15,12 @@ namespace BULKYWEB.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public UserController(ApplicationDbContext unitOfWork)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
-            _context = unitOfWork;
+            _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -23,11 +29,81 @@ namespace BULKYWEB.Areas.Admin.Controllers
                 .Include(ap => ap.Company)
                 .ToList();
 
-
-
-
             return View(appUser);
         }
+
+
+
+
+        public IActionResult RoleManagement(string userId)
+        {
+
+            string RoleId = _context.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagmentVM RoleVM = new RoleManagmentVM()
+            {
+                ApplicationUser = _context.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _context.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _context.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _context.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+
+            return View(RoleVM);
+        }
+
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagmentVM RoleVM)
+        {
+
+            string RoleId = _context.UserRoles.FirstOrDefault(u => u.UserId == RoleVM.ApplicationUser.Id).RoleId;
+            string oldRole = _context.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+            if (!(RoleVM.ApplicationUser.Role == oldRole))
+            {
+                ApplicationUser applicationUser = _context.ApplicationUsers
+                    .FirstOrDefault(ap => ap.Id == RoleVM.ApplicationUser.Id);
+
+
+                if (RoleVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = RoleVM.ApplicationUser.CompanyId;
+                }
+
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+
+
+                _context.SaveChanges();
+
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, RoleVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+             }
+
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+
+
 
 
         #region Api Calls
@@ -72,14 +148,36 @@ namespace BULKYWEB.Areas.Admin.Controllers
 
 
 
-        [HttpDelete]
-        public IActionResult Delete(int? id)
+        [HttpPost]
+        public IActionResult LockUnLock([FromBody] string id)
         {
 
+
+            var objDb = _context.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+
+            if (objDb == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Failed To Deleting"
+                });
+            }
+
+            if (objDb.LockoutEnd != null && objDb.LockoutEnd > DateTime.Now)
+            {
+                objDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            _context.SaveChanges();
             return Json(new
             {
                 success = true,
-                message = "Success To Deleting"
+                message = "Success Operation"
             });
         }
 
